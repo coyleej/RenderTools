0d52e8fdd4c9278358abeb54e1618aa3a7e40c55
Eleanor Coyle <coyleej@protonmail.com>
2019-01-10 17:19:15 -0500

    Added support for rectangles, ellipses, and polygons. Only polygons are untested. All other shapes have been tested and are fully functional.
    Added a color dictionary for automatically assigning colors.
    
    Added SiO2 and Si colors and finishes. SiO2 good to go, but Si still needs work.
    
    Tweeked the glass and billiard finishes to the point where I'm happy about them.

--- util_pov.py
+++ util_pov.py
@@ -1,28 +1,60 @@
 #### ---- NEW FUNCTIONS ---- ####
 
 def create_cylinder(center, end, radius, for_silo=False):
-    cyl_string = "cylinder {ob:c} ".format(ob=123) \
-            + "<{0}, {1}, {2}>, ".format(center[0], center[1], end[0]) \
-            + "<{0}, {1}, {2}>, ".format(center[0], center[1], end[1]) 
+    """ Creates a circular pillar """
+    cyl_string = "cylinder \n\t\t{ob:c}\n\t\t ".format(ob=123) \
+            + "<{0}, {1}, {2}>, \n\t\t".format(center[0], center[1], end[0]) \
+            + "<{0}, {1}, {2}>, \n\t\t".format(center[0], center[1], end[1]) 
     if for_silo:
         cyl_string += "{0} {cb:c}\n\t\t".format(radius, cb=125)
     else:
         cyl_string += "{0}\n\t\t".format(radius)
     return cyl_string
 
-def create_rectangle(center, end, halfwidth, for_silo=False):
-    print("WARNING: create_rectangle function has not been tested!!")
+def create_ellipse(center, end, halfwidths, angle=0, for_silo=False):
+    """ Creates an elliptical pillar """
+    ellipse_string = "cylinder \n\t\t{ob:c}\n\t\t ".format(ob=123) \
+            + "<{0}, {1}, {2}>, \n\t\t".format(center[0], center[1], end[0]) \
+            + "<{0}, {1}, {2}>, 1 \n\t\t".format(center[0], center[1], end[1]) \
+            + "scale <{0}, {1}, 1> \n\t\t".format(halfwidths[0], halfwidths[1])
+    if angle != 0:      # in degrees
+        ellipse_string += "rotate <0, 0, {0}> \n\t\t".format(angle)
+    if for_silo:
+        ellipse_string += "{cb:c}\n\t\t".format(cb=125)
+    return ellipse_string
+
+def create_rectangle(center, end, halfwidths, angle=0, for_silo=False):
+    """ Creates a rectangular box """
     rect_string = "box\n\t\t{ob:c}\n\t\t".format(ob=123) \
-            + "<{0}, ".format(center[0] - halfwidth[0]) \
-            + "{0}, {1}>\n\t\t".format((center[1] - halfwidth[1]), end[0]) \
-            + "<{0} ".format(center[0] + halfwidth[0]) \
-            + "{0}, {1}>\n\t\t".format((center[1] + halfwidth[1]), end[1])
+            + "<{0}, ".format(center[0] - halfwidths[0]) \
+            + "{0}, {1}>\n\t\t".format((center[1] - halfwidths[1]), end[0]) \
+            + "<{0} ".format(center[0] + halfwidths[0]) \
+            + "{0}, {1}>\n\t\t".format((center[1] + halfwidths[1]), end[1])
+    if angle != 0:      # in degrees
+        rect_string += "rotate <0, 0, {0}> \n\t\t".format(angle)
     if for_silo:
         rect_string += "{cb:c}\n\t\t".format(cb=125)
     return rect_string
 
+def create_polygon(center, end, num_points, points, angle=0, for_silo=False):
+    """ Creates a polygon. CURRENTLY UNTESTED!!"""
+    print("\ncreate_polygon is untested!\n")
+    poly_string = "prism\n\t\t{ob:c}\n\t\t".format(ob=123) \
+            + "linear_sweep \n\t\tlinear_spline \n\t\t" \
+            + "{0}, {1}, {2} \n\t\t".format(num_points, end[0], end[1])
+    for i in range(len(num_points)):
+        poly_string += "<{0}, {1}>, ".format(points[i][0], points[i][1])
+    poly_string += "<{0}, {1}> ".format(points[0][0], points[0][1])
+    if center != [0.0, 0.0]:
+        poly_string += "translate <{0}, {1}, 0> \n\t\t".format(center[0], center[1])
+    if angle != 0:      # in degrees
+        poly_string += "rotate <0, 0, {0}> \n\t\t".format(angle)
+    if for_silo:
+        poly_string += "{cb:c}\n\t\t".format(cb=125)
+    return
+
 def update_device_dims(device_dims, new_x, new_y, new_z):
-    """ tracks overall device dimensions to aid in guessing camera placement"""
+    """ Tracks overall device dimensions to aid in guessing camera placement"""
     device_dims[0] = max(new_x, device_dims[0])
     device_dims[1] = max(new_y, device_dims[1])
     #device_dims[2] = max(new_z, device_dims[2])
@@ -39,7 +71,7 @@ def guess_camera(device_dims, center, camera_style="perspective", angle=0):
     light_position = [0, 0, 0]
 
     deg_to_rads = pi / 180.0
-    angle = deg_to_rads * angle  # to radians!
+    angle *= deg_to_rads    # to radians!
 
     if camera_style == "perspective":
         x_offset = 1.2
@@ -48,15 +80,16 @@ def guess_camera(device_dims, center, camera_style="perspective", angle=0):
         print("WARNING: Camera parameters have not been optimized for this style!")
 
     camera_offset = x_offset * max(device_dims) 
+    camera_z_scale = 1.0            # was 0.5 
 
     camera_position[0] = (camera_offset + device_dims[0]) * cos(angle)
     camera_position[1] = (camera_offset + device_dims[0]) * sin(angle)
-    camera_position[2] = 0.5 * (device_dims[2])            # was 0.4
+    camera_position[2] = camera_z_scale * (device_dims[2])
     camera_look_at = [center[0], center[1], (-0.66 * device_dims[2])]
 
     light_offset = camera_offset * 1.25 #* 4.0/3.0
-    light_position[0] = (device_dims[0] + light_offset) * cos(angle - 10 * deg_to_rads)
-    light_position[1] = (device_dims[1] + light_offset/1.0) * sin(angle - 10 * deg_to_rads)
+    light_position[0] = (device_dims[0] + light_offset) * cos(angle - 12 * deg_to_rads)
+    light_position[1] = (device_dims[1] + light_offset/1.0) * sin(angle - 12 * deg_to_rads)
     #light_position[2] = device_dims[2] + light_offset/3.0
     light_position[2] = camera_position[2] + light_offset/3.0
 
@@ -75,42 +108,76 @@ def color_and_finish(dev_string, color, filter_ = 0, transmit = 0, finish = "dul
     POVRay finish examples:
     http://www.povray.org/documentation/view/3.6.0/79/"""
 
-    if finish == "glass":
+    if finish == "Si" or finish == "silicon":
+        filter_ = 0.5
+        transmist = 1.5
+        extra_finish = "finish \n\t\t\t{ob:c} \n\t\t\t".format(ob=123) \
+                + "ambient 0.1 \n\t\t\t" \
+                + "diffuse 0.1 \n\t\t\t" \
+                + "specular 1.0 \n\t\t\t" \
+                + "brilliance 5 \n\t\t\t" \
+                + "roughness 0.001 \n\t\t\t" \
+                + "reflection 0.4 metallic \n\t\t\t" \
+                + "{cb:c}\n\t\t".format(cb=125) \
+                + "interior {ob:c} ior 4.24 {cb:c}\n\t\t".format(ob=123, cb=125)
+                # IOR taken from blender documentation: https://docs.blender.org/manual/en/latest/render/blender_render/materials/properties/transparency.html#examples
+
+    elif finish == "SiO2":
+        extra_finish = ""
+        filter_ = 0.98
+        extra_finish = "finish \n\t\t\t{ob:c} \n\t\t\t".format(ob=123) \
+                + "specular 0.6 \n\t\t\t" \
+                + "phong 0.8 \n\t\t\t" \
+                + "brilliance 5 \n\t\t\t" \
+                + "reflection {ob:c} 0.0, 1.0 fresnel on {cb:c}\n\t\t\t".format(ob=123, cb=125) \
+                + "{cb:c}\n\t\t".format(cb=125) \
+                + "interior {ob:c} ior 1.45 {cb:c}\n\t\t".format(ob=123, cb=125)
+
+    elif finish == "glass":
         filter_ = 0.95
         extra_finish = "finish \n\t\t\t{ob:c} \n\t\t\t".format(ob=123) \
+                + "specular 0.6 \n\t\t\t" \
                 + "phong 0.8 \n\t\t\t" \
-                + "reflection 0.2 \n\t\t\t" \
+                + "brilliance 5 \n\t\t\t" \
+                + "reflection {ob:c} 0.2, 1.0 fresnel on {cb:c}\n\t\t\t".format(ob=123, cb=125) \
                 + "{cb:c}\n\t\t".format(cb=125) \
                 + "interior {ob:c} ior 1.5 {cb:c}\n\t\t".format(ob=123, cb=125)
+                #+ "phong 0.8 \n\t\t\t" \
+                #+ "reflection 0.2 \n\t\t\t" \
+                #+ "{cb:c}\n\t\t".format(cb=125) \
+                #+ "interior {ob:c} ior 1.5 {cb:c}\n\t\t".format(ob=123, cb=125)
 
     elif finish == "metal":
         extra_finish = "finish \n\t\t\t{ob:c} \n\t\t\t".format(ob=123) \
-                + "ambient 0.1 \n\t\t\t" \
+                + "emission 0.1 \n\t\t\t" \
                 + "diffuse 0.1 \n\t\t\t" \
                 + "specular 1.0 \n\t\t\t" \
                 + "roughness 0.001 \n\t\t\t" \
                 + "reflection 0.5 metallic \n\t\t\t" \
                 + "{cb:c}\n\t\t".format(cb=125)
+                #+ "ambient 0.1 \n\t\t\t" \ # ambient replaced by emission in 3.7
 
     elif finish == "irid":
         filter_ = 0.7
         extra_finish = "finish \n\t\t\t{ob:c} \n\t\t\t".format(ob=123) \
-                + "phong 0.1 \n\t\t\t" \
+                + "phong 0.5 \n\t\t\t" \
                 + "reflection {ob:c} 0.2 metallic {cb:c}\n\t\t\t".format(ob=123, cb=125) \
                 + "diffuse 0.3 \n\t\t\t" \
                 + "irid {ob:c} 0.75 thickness 0.5 ".format(ob=123) \
                 + "turbulence 0.5 {cb:c}\n\t\t\t".format(cb=125) \
                 + "{cb:c}\n\t\t".format(cb=125) \
                 + "interior {ob:c} ior 1.5 {cb:c}\n\t\t".format(ob=123, cb=125)
+                #+ "phong 0.1 \n\t\t\t" \
 
     elif finish == "billiard":
         extra_finish = "finish \n\t\t\t{ob:c} \n\t\t\t".format(ob=123) \
-                + "ambient 0.8 \n\t\t\t" \
-                + "diffuse 1 \n\t\t\t" \
-                + "specular 1 \n\t\t\t" \
+                + "ambient 0.3 \n\t\t\t" \
+                + "diffuse 0.8 \n\t\t\t" \
+                + "specular 0.2 \n\t\t\t" \
                 + "roughness 0.005 \n\t\t\t" \
                 + "metallic 0.5 \n\t\t\t" \
                 + "{cb:c}\n\t\t".format(cb=125)
+                #+ "ambient 0.8 \n\t\t\t" \ # ambient replaced by emission in 3.7
 
     # color declaration for ALL finishes
     dev_string += "pigment {ob:c} ".format(ob=123) \
--- write_POV.py
+++ write_POV.py
@@ -2,27 +2,38 @@
 # ~/Code/PovrayTools/write_POV.py
 # ~/Code/S4Signac/device_init.json
 # ~/Code/S4Signac/simulate.py
-# povray difference: http://wiki.povray.org/content/Reference:Difference
 # example: http://wiki.povray.org/content/Documentation:Tutorial_Section_2#Basic_Shapes
 
 from os import system
 import signac
 from util_pov import update_device_dims, guess_camera, color_and_finish
-from util_pov import create_cylinder, create_rectangle
+from util_pov import create_cylinder, create_ellipse, create_rectangle, create_polygon
 from util import deep_access
 
 """ Generates a .pov and optionally render an image for a single device """
 
-# CYLINDER
-json_file = "DeviceFiles/Cylinders/device.index.json.gz"
+# RECTANGLE
+json_file = "DeviceFiles/Rectangles/device.index.json.gz"
 device_id = "318a5dce269fc505ef665148c36a7677"
+
+# CYLINDER
+#json_file = "DeviceFiles/Cylinders/device.index.json.gz"
+#device_id = "318a5dce269fc505ef665148c36a7677"
+
+# ELLIPSE
+#json_file = "DeviceFiles/Ellipse/device.index.json.gz"
+#device_id = "318a5dce269fc505ef665148c36a7677"
 
 # SILO
 #json_file = "DeviceFiles/Silos/device.index.json.gz"
 #device_id = "698bd2fc89cbb7439c2268a564569811"
 
 #MOTHEYE
+#json_file = "DeviceFiles/MothEye/device.index.json.gz"
+#device_id = "12b881f6b38c677000cf1e85818a0332"     # original device
+#device_id = "295ff62a2266c881b2bd83084cd8be43"      # my modified device
 
+####################################################
 pov_name = "temp.pov"
 image_name = "render.png"
 
@@ -38,21 +49,25 @@ camera_style = "perspective"
 #   for orthographic you might have to use e.g. "orthographic angle 30"
 shadowless = False
 
-bg_color = [1,1,1]
+bg_color = [1.0, 1.0, 1.0]
 transparent = True
 antialias = True 
 
-height = 800
-width = 800
+height = 900
+width = 1200
 
-num_UC_x = 4
-num_UC_y = 4
+num_UC_x = 6
+num_UC_y = 6
 
 display = False
 render = True
 open_png = True
 
 color = [[1, 0, 1], [0, 1, 1], [1, 0, 0], [0, 1, 0], [1, 1, 0], [0, 0, 1]]
+Si_color = [0.196, 0.298, 0.525]
+SiO2_color = [1.0, 1.0, 0.96]
+
+color_dict = {"subst": [0, 0, 0,6], "Si":[0.196, 0.298, 0.525], "SiO2":[0.99, 0.99, 0.93], "test":[1, 0, 1]}
 
 # FILE INPUT/OUTPUT
 
@@ -63,11 +78,14 @@ fID = open(pov_name,'w')
 
 # CREATE DEVICE
 
-device = ""             # stores the device
 device_center = [0, 0]  # location in xy-plane
 device_dims = [0, 0, 0] # maximum dimensions of the final device
-                        # All components must be positive
-                        # update after adding each layer
+                        # All components must be positive; update after adding each layer
+
+device = ""             # stores the device
+device += "#include \"colors.inc\"\n" \
+        + "#include \"textures.inc\"\n"
+# default include files are in /usr/share/povray-3.7/include/
 
 # Lattice vectors
 lattice_dict = deep_access(device_dict, ['statepoint', 'lattice_vecs'])
@@ -85,13 +103,17 @@ for v in ['a', 'b']:
 
 # Device layers
 device += "#declare UnitCell = "
-device += "union\n\t{ob:c}\n\t".format(ob=123)
+device += "merge\n\t{ob:c}\n\t".format(ob=123)
+#device += "union\n\t{ob:c}\n\t".format(ob=123)
 
 for i in range(deep_access(device_dict, ['statepoint', 'num_layers'])):
-    thickness = deep_access(device_dict, ['statepoint', 'dev_layers', str(i), 'thickness'])
+#    thickness = deep_access(device_dict, ['statepoint', 'dev_layers', str(i), 'thickness'])
 
     if deep_access(device_dict, ['statepoint', 'dev_layers', str(i)]).get('shapes') is not None:
         shapes = deep_access(device_dict, ['statepoint', 'dev_layers', str(i), 'shapes'])
+        thickness = deep_access(device_dict, ['statepoint', 'dev_layers', str(i), 'thickness'])
+
+        print("layer ", str(i), "\nthickness ", thickness)
 
         # Determine shape(s) in layer
         # WARNING: CURRENTLY VERY RUDIMENTARY!
@@ -109,7 +131,8 @@ for i in range(deep_access(device_dict, ['statepoint', 'num_layers'])):
             else:
                 print("ERROR: This shape is not currently supported!")
         elif len(shapes) == 2:
-            if deep_access(shapes, [str(k), 'material']) != "Vacuum" and deep_access(shapes, [str(k+1), 'material']) == "Vacuum": 
+            if deep_access(shapes, [str(k), 'material']) != "Vacuum" and \
+                    deep_access(shapes, [str(k+1), 'material']) == "Vacuum": 
                 layer_type = "silo"
             else:
                 print("ERROR: This shape is not currently supported!")
@@ -125,7 +148,7 @@ for i in range(deep_access(device_dict, ['statepoint', 'num_layers'])):
             end = [(-1.0 * device_dims[2]), (-1.0 * device_dims[2] - thickness)]
 
             device += create_cylinder(center, end, radius)
-            device = color_and_finish(device, color[i], finish = "irid")
+            device = color_and_finish(device, color_dict["test"], finish = "glass")
 
             device_dims = update_device_dims(device_dims, radius, radius, thickness)
 
@@ -164,28 +187,36 @@ for i in range(deep_access(device_dict, ['statepoint', 'num_layers'])):
             else:
                 print("ERROR: This shape is not supported!!")
 
-            device = color_and_finish(device, color[i], finish = "irid")
+            #device = color_and_finish(device, color[i], finish = "irid")
+            #device = color_and_finish(device, color[i], finish = "glass")
+            device = color_and_finish(device, color_dict["SiO2"], finish = "SiO2")
 
             device_dims = update_device_dims(device_dims, radius_0, radius_0, thickness)
 
         elif layer_type == "ellipse":
             material = deep_access(shapes, [str(k), 'material'])
-            shape_0 = ""
+            center = deep_access(shapes, [str(k), 'shape_vars', 'center'])
+            halfwidths = deep_access(shapes, [str(k), 'shape_vars', 'halfwidths'])
+            angle = deep_access(shapes, [str(k), 'shape_vars', 'angle'])
+            end = [(-1.0 * device_dims[2]), (-1.0 * device_dims[2] - thickness)]
+
+            device += create_ellipse(center, end, halfwidths, angle)
+            device = color_and_finish(device, color_dict["SiO2"], finish = "glass")
+
+            device_dims = update_device_dims(device_dims, halfwidths[0], halfwidths[1], thickness)
+
 
         elif layer_type == "rectangle":
             material = deep_access(shapes, [str(k), 'material'])
             center = deep_access(shapes, [str(k), 'shape_vars', 'center'])
-            halfwidth = deep_access(shapes, [str(k+1), 'shape_vars', 'halfwidth'])
+            halfwidths = deep_access(shapes, [str(k), 'shape_vars', 'halfwidths'])
+            angle = deep_access(shapes, [str(k), 'shape_vars', 'angle'])
             end = [(-1.0 * device_dims[2]), (-1.0 * device_dims[2] - thickness)]
 
-            device += create_rectangle(center, end, halfwidth)
-#            device += "box\n\t\t{ob:c}\n\t\t".format(ob=123) \
-#                + "<{0}, {1}, {2}>\n\t\t".format((center[0] - halfwidth[0]), (center[1] - halfwidth[1]), end[0]) \
-#                + "<{0}, {1}, {2}>\n\t\t".format((center[0] + halfwidth[0]), (center[1] + halfwidth[1]), end[1])
+            device += create_rectangle(center, end, halfwidths, angle)
+            device = color_and_finish(device, color_dict["test"], finish = "irid")
 
-            device = color_and_finish(device, color[i], finish = "glass")
-
-            device_dims = update_device_dims(device_dims, halfwidth[0], halfwidth[1], thickness)
+            device_dims = update_device_dims(device_dims, halfwidths[0], halfwidths[1], thickness)
 
         else:
             print("\nWARNING: Invalid or unsupported layer specified.\n")
@@ -201,7 +232,8 @@ thickness_sub = max(1, deep_access(device_dict, ['statepoint', 'sub_layer', 'thi
 background_sub = deep_access(device_dict, ['statepoint', 'sub_layer', 'background'])
 halfwidth = [(0.5 * lattice_vecs[0][0]), (0.5 * lattice_vecs[1][1])]
 
-color = [0, 0, 1]
+color = [0, 0, 0.8]
+#color = [0, 0, 0]
 
 end_0 = [(-1.0 * device_dims[2]), (-1.0 * device_dims[2] - thickness_sub)]
 
@@ -220,26 +252,30 @@ device += "{cb:c}\n\n".format(cb=125)
 #device += "object {ob:c} UnitCell {cb:c}\n".format(ob=123, cb=125)
 
 # For a bunch of them
-#lattice_vecs = [[,], [,]]
-#num_UC_x = 4
-#num_UC_y = 4
-
 device += "merge\n\t{ob:c} \n\t".format(ob=123)
+
+# Shift translation so that the original device is roughly in the center
+# subtracts 1 because one row stays at origin
+# uses modulo to subtract again if odd number
+# sends half of the remaining rows backward
+adj_x = int(0.5 * (num_UC_x - (1 + (num_UC_x - 1) % 2)))
+adj_y = int(0.5 * (num_UC_y - (1 + (num_UC_y - 1) % 2)))
 
 for i in range(num_UC_x):
     for j in range(num_UC_y):
-        device += "object {ob:c} UnitCell translate <{0}, {1}, {2}> {cb:c}\n\t".format((i * lattice_vecs[0][0]), (j * lattice_vecs[1][1]), 0, ob=123, cb=125)
+        device += "object {ob:c} UnitCell translate <{0}, {1}, {2}> {cb:c}\n\t".format( \
+                ((i - adj_x) * lattice_vecs[0][0]), ((j - adj_y) * lattice_vecs[1][1]), 0, ob=123, cb=125)
+                #(i * lattice_vecs[0][0]), (j * lattice_vecs[1][1]), 0, ob=123, cb=125)
 
 device += "{cb:c}\n\n".format(cb=125)
 
-device_dims = update_device_dims(device_dims, (num_UC_x*device_dims[0]), (num_UC_y * device_dims[1]), device_dims[2])
-
-print("device:")
-print(device)
+device_dims = update_device_dims(device_dims, (num_UC_x * device_dims[0]), \
+        (num_UC_y * device_dims[1]), device_dims[2])
 
 ## HEADER AND CAMERA INFO
 if camera_loc == [] or look_at == [] or light_loc == []:
-    camera_loc, look_at, light_loc = guess_camera(device_dims, device_center, camera_style, angle = camera_angle)
+    camera_loc, look_at, light_loc = \
+            guess_camera(device_dims, device_center, camera_style, angle = camera_angle)
 
 if camera_style == "":
     camera_style = "perspective"
