f701df6ccb5f47c6620b12e27841f55c536b85fb
Eleanor Coyle <coyleej@protonmail.com>
2019-03-20 13:38:27 -0400

    Major cleanup and modification of call_gif_POV.py.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# S4S-PovrayTools
-POV-Ray tools for use with S4Signac
+# RenderTools
+POV-Ray tools for use with MANTIS
+
+This repository is a stand-alone development branch for the MANTIS rendering tools. It is actively under development, so some of the scripts may be semi-functional. If you want something that is guaranteed to work, please refer to MANTIS. (MANTIS can be found here: https://github.com/harperes/MANTIS) 
+
+The following is a brief description of what all files here do. For additional details, please refer to the docstrings in the functions.
+
+call_write_POV.py : example for rendering an image of a single device from a MANTIS json file, calls rendering.py
+
+call_gif_POV.py : example for rendering a gif containing a series of devices from a MANTIS json file, calls rendering.py
+
+rendering.py : the POV-Ray function writes a .pov file and renders a .png by default (will eventually a blender function, too)
+
+util.py : the function to extract data from the MANTIS json
+
+util_pov.py : utility functions required by the code in rendering.py
+
--- call_gif_POV.py
+++ call_gif_POV.py
@@ -1,31 +1,43 @@
 import signac
 from rendering import write_pov
-from util_pov import guess_camera
 from os import system
 
-# RECTANGLE
-#json_file = "DeviceFiles/Rectangles/device.index.json.gz"
+##### File info #####
+json_file = "DeviceFiles/Eric_poster/device.index.json.gz"
+output_dir = "Poster/"
+gif_name = "devices.gif"
 
-# CYLINDER
-#json_file = "DeviceFiles/Cylinders/device.index.json.gz"
+##### Render settings #####
+height = 300
+# width = height #in function call
 
-# ELLIPSE
-#json_file = "DeviceFiles/Ellipse/device.index.json.gz"
+# All have the same camera information
+camera_loc = [3.5, 6.5, 4.0]
+camera_look_at = [0, 0, -2]
+light_loc = [5.9, 6.6, 5.7]
+# cat *pov | grep location | sed -e "s/, / /g" -e "s/ </ /" -e "s/>/ /" | cut -d " " -f 2,3,4 | tee gif_camera_info.txt
 
-# SILO
-json_file = "DeviceFiles/Silos/device.index.json.gz"
+device_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
 
-#MOTHEYE
-#json_file = "DeviceFiles/MothEye/device.index.json.gz"
+num_UC = [20, 18, 16, 14, 12, 10, 9, 8, 24, 20, 20]
 
-# MISC
-#json_file = "DeviceFiles/Test/device.index.json.gz"
+custom_colors = [
+        [0.12156862745098, 0.466666666666667, 0.705882352941176], 
+        [1, 0.498039215686275, 0.054901960784314], 
+        [0.83921568627451, 0.152941176470588, 0.156862745098039], 
+        [0.172549019607843, 0.627450980392157, 0.172549019607843], 
+        [0.549019607843137, 0.337254901960784, 0.294117647058824], 
+        [0.580392156862745, 0.403921568627451, 0.741176470588235], 
+        [0.090196078431373, 0.745098039215686, 0.811764705882353], 
+        [0.737254901960784, 0.741176470588235, 0.133333333333333], 
+        [0.83921568627451, 0.152941176470588, 0.156862745098039], 
+        [0.12156862745098, 0.466666666666667, 0.705882352941176], 
+        [0.12156862745098, 0.466666666666667, 0.705882352941176]
+        ]
 
-####################################################
-output_dir = "silo_test"
-gif_name = "result.gif"
-open_gif = True 
-make_gif = True
+##### Gif settings #####
+make_gif = True 
+open_gif = True
 
 # GIF settings:
 # Number of times to loop before stopping (0 = never stop)
@@ -35,82 +47,43 @@ delay_time = 40
 # Negate image (creates negative by replacing with complimentary color)
 negate = False
 
-#dirname="Devices"; if [ ! -d $dirname ]; then echo "True" && mkdir ${dirname}; fi
-command = "if [ ! -d {0:s} ]; then mkdir -p {0:s}; fi".format(output_dir)
-print(command)
-system(command)
+##### Renders stuff #####
+for i in range(len(device_list)):
+    device_id = device_list[i]
 
-limit = 15       #  number of data points to load
+    pov_name = output_dir + device_list[i] + ".pov"
+    image_name = output_dir + device_list[i] + ".png"
 
-height = 600
-num_UC = 4
-
-# List of IDs doesn't always have the same order!
-# For actual application, the user must feed in their own list of
-# device names. This is just for testing
-with signac.Collection.open(json_file, compresslevel=1) as d_index:
-    id_list = [doc.get("_id") for doc in d_index.find(limit=limit)]
-
-#print(id_list)
-
-# Guess camera, so that all images use the same camera settings
-#
-# NEED TO ADD THIS!!
-# camera_position, camera_look_at, light_position
-#
-camera_loc = [2.8, 4.5, 2.9]
-camera_look_at = [0, 0, -2]
-light_loc = [4.2, 4.4, 4]
-# cat *pov | grep location | sed -e "s/, / /g" -e "s/ </ /" -e "s/>/ /" | cut -d " " -f 2,3,4 | tee gif_camera_info.txt 
-
-extra_finish = "finish \n\t\t\t{ob:c} \n\t\t\t".format(ob=123) \
-        + "emission 0.2 \n\t\t\t" \
-        + "diffuse 0.3 \n\t\t\t" \
-        + "specular 0.8 \n\t\t\t" \
-        + "roughness 0.01 \n\t\t\t" \
-        + "reflection <0, 0, 1>  metallic \n\t\t\t" \
-        + " metallic \n\t\t\t" \
-        + "{cb:c}\n\t\t".format(cb=125)
-
-custom_colors = [
-#        [1.000, 0.000, 0.000],
-#        [1.000, 0.506, 0.000], 
-#        [1.000, 0.957, 0.000], 
-#        [0.027, 0.824, 0.804], 
-#        [0.024, 0.678, 1.000], 
-        [0.212, 0.211, 0.835],
-        [0.000, 0.000, 1.000], 
-        [1.000, 0.000, 0.000]
-        ]
-
-#if pov_name:
-for i in range(len(id_list)):
-    device_id = id_list[i]
-
+    # Open device dictionary
     with signac.Collection.open(json_file, compresslevel=1) as d_index:
         device_dict = list(d_index.find(filter={"_id": device_id}))[0]
 
+    extra_finish = "finish \n\t\t\t{ob:c} \n\t\t\t".format(ob=123) \
+            + "emission 0.2 \n\t\t\t" \
+            + "diffuse 0.3 \n\t\t\t" \
+            + "specular 0.8 \n\t\t\t" \
+            + "roughness 0.01 \n\t\t\t" \
+            + "reflection <0, 0, 1>  metallic \n\t\t\t" \
+            + " metallic \n\t\t\t" \
+            + "{cb:c}\n\t\t".format(cb=125)
 
-    pov_name = output_dir + "/{:03d}.pov".format(i + 1)
-    image_name = output_dir + "/{:03d}.png".format(i + 1)
-    #print(device_dict)
-
+    # The brackets around custom_colors[i] are required here because 
+    # write_pov expects [[], [], ...], but we're only passing in one color.
     write_pov(device_dict, pov_name, image_name, 
             height = height, width = height, 
-            num_UC_x = num_UC, num_UC_y = num_UC, 
+            num_UC_x = num_UC[i], num_UC_y = num_UC[i], 
             camera_style = "perspective", 
             camera_rotate = 60, ortho_angle = 30, 
             camera_loc = camera_loc,
-            look_at = camera_look_at,
-            light_loc = light_loc,
+            look_at = camera_look_at, 
+            light_loc = light_loc, 
             add_edge_buffer = False, 
-            use_default_colors = False, custom_colors = custom_colors, 
+            use_default_colors = False, custom_colors = [custom_colors[i]], 
             use_finish = "dull", custom_finish = extra_finish, 
-            display = False, render = True , num_threads = 2,
+            display = False, render = True , num_threads = 3, 
             open_png = False)
 
-    #print("{:03d}".format(i + 1))
-
+##### Gif #####
 command = "cd {0:s}; convert ".format(output_dir)
 command += "-delay {0} -loop {1} ".format(delay_time, num_loop)
 if negate:
@@ -122,3 +95,5 @@ if open_gif == True:
 
 if make_gif == True:
     system(command)
+
+print("All done :)")
